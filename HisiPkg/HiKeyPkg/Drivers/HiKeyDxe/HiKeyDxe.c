50312bdcf9ce6c04e8ab032352e76e43fd591627
Haojian Zhuang <haojian.zhuang@linaro.org>
2015-05-28 13:27:59 +0800

    hikey: load fdt path from variable
    "Update FDT path" menu could set "Fdt" variable in UEFI. So append the
    platform driver to load "Fdt" variable if it's specified.
    
    Then we could discard "dtb=hi6220-hikey.dtb" from kernel command line if
    "Fdt" variable is already specified.
    
    But we can't boot kernel successfully if "Fdt" variable is not set in
    UEFI.
    
    Signed-off-by: Haojian Zhuang <haojian.zhuang@linaro.org>

---
+++ HisiPkg/HiKeyPkg/Drivers/HiKeyDxe/HiKeyDxe.c
@@ -0,0 +1,33 @@
+/** @file
+*
+*  Copyright (c) 2015, Linaro Ltd. All rights reserved.
+*  Copyright (c) 2015, Hisilicon Ltd. All rights reserved.
+*
+*  This program and the accompanying materials
+*  are licensed and made available under the terms and conditions of the BSD License
+*  which accompanies this distribution.  The full text of the license may be found at
+*  http://opensource.org/licenses/bsd-license.php
+*
+*  THE PROGRAM IS DISTRIBUTED UNDER THE BSD LICENSE ON AN "AS IS" BASIS,
+*  WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
+*
+**/
+
+//#include <Library/ArmShellCmdLib.h>
+#include "HiKeyDxeInternal.h"
+
+EFI_STATUS
+EFIAPI
+HiKeyEntryPoint (
+  IN EFI_HANDLE         ImageHandle,
+  IN EFI_SYSTEM_TABLE   *SystemTable
+  )
+{
+  EFI_STATUS           Status;
+
+  // Try to install the Flat Device Tree (FDT). This function actually installs the
+  // UEFI Driver Binding Protocol.
+  Status = HiKeyFdtInstall (ImageHandle);
+
+  return Status;
+}
---
+++ HisiPkg/HiKeyPkg/Drivers/HiKeyDxe/HiKeyDxeInternal.h
@@ -0,0 +1,30 @@
+/** @file
+*
+*  Copyright (c) 2015, Linaro Ltd. All rights reserved.
+*  Copyright (c) 2015, Hisilicon Ltd. All rights reserved.
+*
+*  This program and the accompanying materials
+*  are licensed and made available under the terms and conditions of the BSD License
+*  which accompanies this distribution.  The full text of the license may be found at
+*  http://opensource.org/licenses/bsd-license.php
+*
+*  THE PROGRAM IS DISTRIBUTED UNDER THE BSD LICENSE ON AN "AS IS" BASIS,
+*  WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
+*
+**/
+
+#ifndef __HIKEY_DXE_INTERNAL_H__
+#define __HIKEY_DXE_INTERNAL_H__
+
+#include <Uefi.h>
+
+#include <Library/DebugLib.h>
+#include <Library/DxeServicesTableLib.h>
+#include <Library/UefiBootServicesTableLib.h>
+
+EFI_STATUS
+HiKeyFdtInstall (
+  IN EFI_HANDLE                            ImageHandle
+  );
+
+#endif // __HIKEY_DXE_INTERNAL_H__
